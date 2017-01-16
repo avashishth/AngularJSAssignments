33cf5719a2a09ad71b3af9de7d3d656f756f70c8
Ashish Vashishth <ashish.vashishth@gmail.com>
2017-01-16 10:49:55 +0530

    Assignment 2 cut 1
    Assignment 2 cut 1

---
+++ assignment2/scripts/app.js
@@ -0,0 +1,54 @@
+(function () {
+    'use strict';
+    
+    angular.module('assign2App', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+    
+    
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService){
+        var tbCtrl = this;
+        tbCtrl.items = ShoppingListCheckOffService.getToBuyItems();
+        
+        tbCtrl.checkOff = function(index) {
+            ShoppingListCheckOffService.checkOff(index);
+        }
+    };
+    
+    AlreadyBoughtController.$inject= ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var abCtrl = this;
+        abCtrl.items = ShoppingListCheckOffService.getBoughtItems();
+        
+        abCtrl.check = abCtrl.items.length === 0;
+    };
+
+    function ShoppingListCheckOffService() {
+        var service = this;
+        var boughtItems = [];
+        var toBuyItems = [
+            {name: "cookies", quantity: 10},
+            {name: "chips", quantity: 10},
+            {name: "coke", quantity: 10},
+            {name: "cread", quantity: 1},
+            {name: "cheeze", quantity: 1},
+            {name: "candies", quantity: 10}
+        ];
+    
+        service.getToBuyItems = function() {
+            return toBuyItems;  
+        };
+        
+        service.getBoughtItems = function() {
+            return boughtItems;
+        };
+        
+        service.checkOff = function(index) {
+            boughtItems.push(toBuyItems[index]);
+            toBuyItems.splice(index, 1);
+        };
+    };
+    
+})();
